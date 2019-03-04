d4e48d2226b2d0d124eb8bc1fb6fa13a841b5a77
Abe D <iaryiou111@gmail.com>
2019-03-04 03:41:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   split_string = string.split(' ')
-  new_strings = split_string.map(&:reverse)
+  new_strings = split_string.collect(&:reverse)
   output_string = ''
   #new_strings.each.with_index do |word, index|
   #  output_string += word + (index < new_strings.size-1 ? " " : "")
